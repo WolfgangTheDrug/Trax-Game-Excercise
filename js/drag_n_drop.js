84c811502045763635e68cd5edb77abb0a4dd667
WolfgangTheDrug <bardak_widz@wp.pl>
2022-04-15 00:28:44 +0200

    Flip tile on double-click

--- js/drag_n_drop.js
+++ js/drag_n_drop.js
@@ -1,3 +1,6 @@
+const tile = document.querySelector('.tile')
+tile.addEventListener("dblclick", () => tile.classList.toggle("flipped"));
+
 function handleDragStart(e) {
   this.style.filter = "grayscale(50%)";
 }
@@ -11,9 +14,6 @@ function addFlip(e) {
   this.classList.toggle('.flip-vertical-right');
 }
 
-let tile = document.querySelector('.tile');
-
 tile.addEventListener('dragstart', handleDragStart);
 tile.addEventListener('dragend', handleDragEnd);
-
 tile.addEventListener('dblclick', addFlip);
